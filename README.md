59be4c19834394165f266876d0f93ecd81a2eb51
eXtremeSHOK.com <admin@extremeshok.com>
2015-05-14 01:01:35 +0200

    Minor Update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Automated Bayesain Training Script for Spam Assassin
 ===========================
-Train ham and spam, ignore folders with could lead to a possible false positives
+Train ham and spam, ignore folders with could lead to possible false positives
 
 Does each directory in turn, to prevent procwatch from killing sa-learn or overloading the process
--- auto-bayesain-training.sh
+++ auto-bayesain-training.sh
@@ -5,17 +5,13 @@
 # 18 December 2013
 # Copyright (c) Adrian Jon Kriel : root-at-extremecooling-dot-org
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 ####################
 #
-# Train ham and spam, ignore folders with could lead to a possible false positives
+# Train ham and spam, ignore folders with could lead to possible false positives
 # Does each directory in turn, to prevent procwatch from killing sa-learn or overloading the process
 #
 ####################
 
-# Train ham (ignore outgoing mail & deleted mail that may be spam or ham)
-# Do each directory in turn so that procwatch doesn't kill sa-learn
-#
 echo "Training HAM"
 find /var/vmail/vmail1/ -name cur | egrep -i -v '(.spam)|(.trash)|(.sent)|(.junk)|(.deleted)|(.drafts)|(.outbox)' | while read i; do
  /usr/bin/sa-learn --no-sync --ham "$i"
